ec2401d636d76cafdda5fcafdd0556e7546b5ba5
ymat2 <yuki.matsuda.r7@dc.tohoku.ac.jp>
2023-05-23 15:19:43 +0900

    :bug: Conf. on Darwin

--- src/get_sequence.sh
+++ src/get_sequence.sh
@@ -8,4 +8,4 @@ wget https://ftp.ensembl.org/pub/release-109/fasta/canis_lupus_dingo/pep/Canis_l
 gunzip -d Canis_lupus_dingo.ASM325472v1.pep.all.fa.gz
 mv Canis_lupus_dingo.ASM325472v1.pep.all.fa data/
 
-python3 test/get_longest_isoform.py -i data -o data
+python3 src/get_longest_isoform.py -i data -o data
